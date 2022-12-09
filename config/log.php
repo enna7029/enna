2ac1077a118aef3aa81efaf9803e80920849935a
enna <w1069103107@outlook.com>
2022-12-09 14:47:15 +0800

    日志的接入_日志配置

--- config/log.php
+++ config/log.php
@@ -13,8 +13,22 @@ return [
             'type' => 'File',
             //日志记录的路径
             'path' => '',
+            //是否单日志文件
+            'single' => false,
+            //文件大小限制
+            'file_size' => 2097152,
+            //独立日志
+            'apart_level' => [],
+            //最大日志文件数量
+            'max_files' => 0,
+            //json格式
+            'json' => false,
+            //日志格式
+            'format' => '[%s][%s] %s',
             //是否实时写入
             'realtime_write' => false,
+            //是否关闭
+            'close' => false,
         ],
         'socket' => [
             'type' => 'Socket'
